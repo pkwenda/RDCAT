17262e85d118ca998a848ed86e9f9338a09495d7
庄文达 <wenda.zhuang@mendbanks.cn>
2022-01-05 00:07:22 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-app id="inspire">
+    
+ <div class="ma-12 pa-12">
+    <v-card>
+      <!-- left nav -->
+      <v-navigation-drawer
+        permanent
+        expand-on-hover
+        app
+        clipped
+        left
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </div>
+
+    <!-- right nav -->
+    <!-- permanent mean not folding -->
+    <v-navigation-drawer
+      permanent 
+      app
+      clipped
+      right
+    >
+      <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+
+    <v-footer
+      app
+      color="transparent"
+      height="72"
+      inset
+    >
+      <v-text-field
+        background-color="grey lighten-1"
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+      ></v-text-field>
+    </v-footer>
+  </v-app>
+</template>
+ 
+
+<script>
+export default {
+    name: 'Home',
+    data: () => ({ drawer: null }),
+  } 
+</script>
